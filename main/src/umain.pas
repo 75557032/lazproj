df58422004f3dd9d58f13e0e3f172178d3ca2148
75557032 <2524153@163.com>
2013-07-04 22:54:25 +0800

    新建项目

---
+++ main/src/umain.pas
@@ -0,0 +1,29 @@
+unit umain;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics;
+
+type
+
+  { TfrmMain }
+
+  TfrmMain = class(TForm)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
